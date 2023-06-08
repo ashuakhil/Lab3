7293b3396591d3cdaee4ebba01eee5002a07435d
ashuakhil <133909650+ashuakhil@users.noreply.github.com>
2023-06-07 23:51:14 -0400

    scripts

--- process_sales_data.py
+++ process_sales_data.py
@@ -12,6 +12,7 @@ import pandas as pd
 import sys
 import os.path
 from datetime import date
+import re
 
 def main():
     sales_csv_path = get_sales_csv_path()
@@ -71,19 +72,67 @@ def process_sales_data(sales_csv_path, orders_dir_path):
         orders_dir_path (str): Path of orders directory
     """
     # TODO: Import the sales data from the CSV file into a DataFrame
-    # TODO: Insert a new "TOTAL PRICE" column into the DataFrame
-    # TODO: Remove columns from the DataFrame that are not needed
-    # TODO: Groups orders by ID and iterate 
-        # TODO: Remove the 'ORDER ID' column
-        # TODO: Sort the items by item number
-        # TODO: Append a "GRAND TOTAL" row
-        # TODO: Determine the file name and full path of the Excel sheet
-        # TODO: Export the data to an Excel sheet
-        # TODO: Format the Excel sheet
-        # TODO: Define format for the money columns
-        # TODO: Format each colunm
-        # TODO: Close the Excelwriter 
-    return
+    df = pd.read_csv(sales_csv_path)
 
-if __name__ == '__main__':
-    main()
+    # TODO: Insert a new "TOTAL PRICE" column into the DataFrame
+    df.insert(7, 'TOTAL PRICE', df['ITEM QUANTITY'] * df['ITEM PRICE'])
+
+    # TODO: Remove columns from the DataFrame that are not needed
+    df.drop(columns=['ADDRESS', 'CITY', 'STATE', 'POSTAL CODE', 'COUNTRY'], inplace=True)
+
+    # TODO: Groups orders by ID and iterate 
+    for order_id, order_df in df.groupby('ORDER ID'):
+
+        # TODO: Remove the 'ORDER ID' column
+        order_df.drop(columns=['ORDER ID'], inplace=True)
+
+        # TODO: Sort the items by item number
+        order_df.sort_values(by='ITEM NUMBER', inplace=True)
+
+        # TODO: Append a "GRAND TOTAL" row
+        grand_total = order_df['TOTAL PRICE'].sum()
+        grand_total_df =pd.DataFrame({'ITEM PRICE': ['GRAND TOTAL'], 'TOTAL PRICE': [grand_total]})
+        order_df = pd.concat([order_df, grand_total_df])
+
+        # TODO: Determine the file name and full path of the Excel sheet
+        customer_name = order_df['CUSTOMER NAME'].values[0]
+        customer_name = re.sub(r'\w', '', customer_name)
+        order_file = f"Order{order_id}_{customer_name}.xlsx"
+        order_excel_path = os.path.join(orders_dir_path, order_file)
+
+        # TODO: Export the data to an Excel sheet
+        worksheet_name = f'Order #{order_id}'
+       # order_df.to_excel(order_excel_path, index=False, sheet_name=worksheet_name)
+
+       # Create a Pandas Excel writer using XlsxWriter as the engine.
+        writer = pd.ExcelWriter(order_excel_path, engine="xlsxwriter")
+
+       # Convert the dataframe to an XlsxWriter Excel object.
+        order_df.to_excel(writer, sheet_name='Order', index=False)
+
+       # Get the xlsxwriter workbook and worksheet objects.
+        workbook = writer.book
+        worksheet = writer.sheets['Order']
+       
+       # Add some cell formats.
+        money_format = workbook.add_format({'num_format': '$#,##0.00'})
+        
+
+
+      # Note: It isn't possible to format any cells that already have a format such
+      # as the index or headers or any cells that contain dates or datetimes.
+ 
+      # Set the column width and format.
+    worksheet.set_column('H:H', 12, money_format)
+
+
+
+      # Close the Pandas Excel writer and output the Excel file.
+    writer.close()
+        
+      
+
+  
+        
+
+       
